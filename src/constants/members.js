94b31c84c939ea6a19db181ffc95f10f7f5b9d8c
wook95 <uclee95@gmail.com>
2022-02-11 01:39:53 +0900

    add: 목데이터 추가

---
+++ src/constants/members.js
@@ -0,0 +1,24 @@
+const MEMBERS = [
+  {
+    userId: 1,
+    userName: '김개발',
+    profileImage: '',
+  },
+  {
+    userId: 2,
+    userName: '문코딩',
+    profileImage: '',
+  },
+  {
+    userId: 3,
+    userName: '이배포',
+    profileImage: '',
+  },
+  {
+    userId: 4,
+    userName: '나',
+    profileImage: '',
+  },
+];
+
+export default MEMBERS;
