a715a05699e0799d316e371231590a68a1fde1ff
wook95 <uclee95@gmail.com>
2022-02-11 00:22:59 +0900

    fix: 리덕스 미들웨어 동작하도록 수정

--- src/redux/slices/index.js
+++ src/redux/slices/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from '@reduxjs/toolkit';
 import userReducer from 'redux/slices/user';
 
 const rootReducer = combineReducers({
-  userReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,7 @@ import { applyMiddleware, configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import rootReducer from 'redux/slices';
 
-export const store = configureStore(
-  {
-    reducer: rootReducer,
-  },
-  applyMiddleware(logger),
-);
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: [logger],
+});
