27f91165df7c7ccb4adbdb748daa50dd9b4b9a60
wook95 <uclee95@gmail.com>
2022-02-12 02:00:37 +0900

    add: redux로 모달 상태 관리 구현

--- src/redux/slices/index.js
+++ src/redux/slices/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import userReducer from 'redux/slices/user';
+import modalReducer from 'redux/slices/modal';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  modal: modalReducer,
 });
 
 export default rootReducer;
