8d70aabed0a6905af3815978e715e725d2ddc950
Hyun Don Moon <hyundonmoon@gmail.com>
2022-02-11 20:48:56 +0900

    add: Chat 페이지에 사이드바 구현, 작동 확인 [#13]

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,10 +1,17 @@
-import { useEffect } from 'react';
-import useUser from 'hooks/useUser';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import useUser from 'hooks/useUser';
+
+import Sidebar from 'components/sidebar';
 
 function Chat() {
   const user = useUser();
   const navigate = useNavigate();
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setSidebarOpen(prev => !prev);
+  };
 
   useEffect(() => {
     if (!user.isLogin) {
@@ -13,7 +20,12 @@ function Chat() {
     }
   }, [user]);
 
-  return <div>chat</div>;
+  return (
+    <>
+      <Sidebar isOpen={sidebarOpen} toggle={toggleSidebar} />
+      <main></main>
+    </>
+  );
 }
 
 export default Chat;
