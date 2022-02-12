194471ec0c96ccc159b6699c12c74998c3ff2dd8
이경은 <2kyung19@gmail.com>
2022-02-12 15:50:41 +0900

    fix: 채널 prop 변경 [#27]

--- src/constants/channels.js
+++ src/constants/channels.js
@@ -3,9 +3,9 @@ import ControllerIcon from 'assets/icons/ControllerIcon';
 import EarbudsIcon from 'assets/icons/EarbudsIcon';
 
 const CHANNELS = [
-  { name: '게이밍', icon: <ControllerIcon /> },
-  { name: '가상 화폐', icon: <BitcoinIcon /> },
-  { name: '음악', icon: <EarbudsIcon /> },
+  { name: '게이밍', icon: <ControllerIcon />, active: false },
+  { name: '가상 화폐', icon: <BitcoinIcon />, active: false },
+  { name: '음악', icon: <EarbudsIcon />, active: false },
 ];
 
 export default CHANNELS;
